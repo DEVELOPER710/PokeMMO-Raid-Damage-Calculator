919a5772854552be845793ab83c57825a91395fc
DEVELOPER710 <apsmile07@gmail.com>
2026-04-24 20:44:18 +0530

    Add files via upload

---
+++ js/data/sets/gen5.js
@@ -0,0 +1,73 @@
+var SETDEX_BW = {
+	Heatran: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Flash Fire",
+			item: "Leftovers",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Magma Storm", "Power Gem", "Solar Beam", "Earth Power"],
+		},
+	},
+	Cresselia: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Levitate",
+			item: "Expert Belt",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Psychic", "Thunderbolt", "Shadow Ball", "Power Gem"],
+		},
+	},
+	Meloetta: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Serene Grace",
+			item: "Sitrus Berry",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Focus Blast", "Charge Beam", "Shadow Ball", "Psychic"],
+		},
+	},
+	"Meloetta-Pirouette": {
+		"Raid Boss": {
+			level: 100,
+			ability: "Serene Grace",
+			item: "Sitrus Berry",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Shadow Claw", "Drain Punch", "Stone Edge", "Zen Headbutt"],
+		},
+	},
+	Cobalion: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Justified",
+			item: "Expert Belt",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Air Slash", "Flash Cannon", "Sacred Sword", "Stone Edge"],
+		},
+	},
+	Virizion: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Justified",
+			item: "Liechi Berry",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Sacred Sword", "Leaf Blade", "Stone Edge", "X-Scissor"],
+		},
+	},
+	Terrakion: {
+		"Raid Boss": {
+			level: 100,
+			ability: "Justified",
+			item: "Lum Berry",
+			nature: "Hardy",
+			evs: { sp: 252 },
+			moves: ["Earthquake", "Sacred Sword", "Stone Edge", "Poison Jab"],
+		},
+	},
+
+};
